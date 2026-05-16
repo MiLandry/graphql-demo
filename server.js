58f0c606c5fba6645d5ef062ec10ff6d443c658c
Michael Landry <mlandry.cont@creditkarma.com>
2026-05-15 23:45:32 -0400

    timesheet

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server');
 const schema = require('./schema');
 
 async function startServer() {
-  const app = express();
   const server = new ApolloServer({
     schema,
   });
 
-  await server.start();
-  server.applyMiddleware({ app });
-
-  app.listen(4000, () => {
-    console.log('Running a GraphQL API server at localhost:4000/graphql');
-  });
+  await server.listen();
+  console.log('Running a GraphQL API server at localhost:4000/graphql');
 }
 
 startServer();
